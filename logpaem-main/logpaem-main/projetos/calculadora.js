2d778699ece31aac2627bcba9394362fe69e6d6b
andre-luiz-formigari <132684560+andre-luiz-formigari@users.noreply.github.com>
2023-10-21 11:46:24 -0300

    Add files via upload

---
+++ logpaem-main/logpaem-main/projetos/calculadora.js
@@ -0,0 +1,291 @@
+var segundoNumero = 1
+var um = ""
+var dois = ""
+
+function passo0(){
+    if(segundoNumero == 1){
+        um =  um + "0"
+      }
+      else{
+       dois =   dois + "0"
+      }
+}
+function passo1() {
+    if(segundoNumero == 1){
+      um =  um + "1"
+    }
+    else{
+     dois =   dois + "1"
+    }
+}
+
+
+function passo2() {
+    if(segundoNumero ==1){
+      um =  um + "2"
+    }
+    else{
+     dois =   dois + "2"
+    }
+}
+
+
+function passo3() {
+    if(segundoNumero ==1){
+      um =  um + "3"
+    }
+    else{
+     dois =   dois + "3"}
+}
+
+
+function passo4() {
+    if(segundoNumero ==1){
+      um =  um + "4"
+    }
+    else{
+     dois =   dois + "4"
+    }
+}
+
+
+function passo5() {
+    if(segundoNumero ==1){
+      um =  um + "5"
+    }
+    else{
+     dois =   dois + "5"
+    }
+}
+
+
+function passo6() {
+    if(segundoNumero ==1){
+      um =  um + "6"
+    }
+    else{
+     dois =   dois + "6"
+    }
+}
+
+
+function passo7() {
+    if(segundoNumero ==1){
+      um =  um + "7"
+    }
+    else{
+     dois =   dois + "7"
+    }
+}
+
+
+function passo8() {
+    if(segundoNumero ==1){
+      um =  um + "8"
+    }
+    else{
+     dois =   dois + "8"
+    }
+}
+
+
+function passo9() {
+    if(segundoNumero ==1){
+      um =  um + "9"
+    }
+    else{
+     dois =   dois + "9"
+    }
+}
+
+function segundo(){
+    if(segundoNumero = 1){
+        segundoNumero = 2
+    }
+    else{
+        segundoNumero = 1
+    }
+}
+
+function resultado(){
+    var tabela = document.getElementById('texto')
+    if(desisao == 1){
+        total = (Number(um) - Number(dois))
+       tabela.value = total
+    }
+    else if(desisao == 2){
+        total = (Number(um) + Number(dois))
+        tabela.value = total
+    }
+    else if(desisao == 3){
+        total = (Number(um) * Number(dois))
+        tabela.value = total
+    }
+    else if(desisao == 4){
+        total = (Number(um) / Number(dois))
+        tabela.value = total
+    }
+    
+}
+function apagar(){
+    var tabela = document.getElementById('texto')
+    um = ""
+    dois = ""
+    segundoNumero = 1
+    tabela.value = ""
+}
+
+//function soma(um,dois){
+ ////alert(uma + dois)
+///}
+
+//function subtraçao(um,dois){
+//    alert(um - dois)
+////}
+//function divisao(um, dois){
+ //  alert( um / dois)
+//}
+//function multiplicaçao(um,dois){
+///    alert(um * dois)
+//}
+
+
+
+
+
+// var segundoNumero = 1
+// var um = ""
+// var dois = ""
+
+// function passo0(){
+//     if(segundoNumero == 1){
+//         um =  um + "0"
+//       }
+//       else{
+//        dois =   dois + "0"
+//       }
+// }
+// function passo1() {
+//     if(segundoNumero == 1){
+//       um =  um + "1"
+//     }
+//     else{
+//      dois =   dois + "1"
+//     }
+// }
+
+
+// function passo2() {
+//     if(segundoNumero ==1){
+//       um =  um + "2"
+//     }
+//     else{
+//      dois =   dois + "2"
+//     }
+// }
+
+
+// function passo3() {
+//     if(segundoNumero ==1){
+//       um =  um + "3"
+//     }
+//     else{
+//      dois =   dois + "3"}
+// }
+
+
+// function passo4() {
+//     if(segundoNumero ==1){
+//       um =  um + "4"
+//     }
+//     else{
+//      dois =   dois + "4"
+//     }
+// }
+
+
+// function passo5() {
+//     if(segundoNumero ==1){
+//       um =  um + "5"
+//     }
+//     else{
+//      dois =   dois + "5"
+//     }
+// }
+
+
+// function passo6() {
+//     if(segundoNumero ==1){
+//       um =  um + "6"
+//     }
+//     else{
+//      dois =   dois + "6"
+//     }
+// }
+
+
+// function passo7() {
+//     if(segundoNumero ==1){
+//       um =  um + "7"
+//     }
+//     else{
+//      dois =   dois + "7"
+//     }
+// }
+
+
+// function passo8() {
+//     if(segundoNumero ==1){
+//       um =  um + "8"
+//     }
+//     else{
+//      dois =   dois + "8"
+//     }
+// }
+
+
+// function passo9() {
+//     if(segundoNumero ==1){
+//       um =  um + "9"
+//     }
+//     else{
+//      dois =   dois + "9"
+//     }
+// }
+
+// function segundo(){
+//     if(segundoNumero = 1){
+//         segundoNumero = 2
+//     }
+//     else{
+//         segundoNumero = 1
+//     }
+// }
+
+// function resultado(){
+//     var tabela = document.getElementById('texto')
+//     if(desisao == 1){
+//         total = (Number(um) - Number(dois))
+//        tabela.value = total
+//     }
+//     else if(desisao == 2){
+//         total = (Number(um) + Number(dois))
+//         tabela.value = total
+//     }
+//     else if(desisao == 3){
+//         total = (Number(um) * Number(dois))
+//         tabela.value = total
+//     }
+//     else if(desisao == 4){
+//         total = (Number(um) / Number(dois))
+//         tabela.value = total
+//     }
+    
+// }
+// function apagar(){
+//     var tabela = document.getElementById('texto')
+//     um = ""
+//     dois = ""
+//     segundoNumero = 1
+//     tabela.value = ""
+// }
